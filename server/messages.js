90a08876661c940630d92822cdfd15218109a11d
Frédéric Renken <renken@users.noreply.github.com>
2016-10-03 15:18:54 -0700

    added testing messages object

---
+++ server/messages.js
@@ -0,0 +1,22 @@
+var messages = {
+  results: [
+    {
+      createdAt: '2016-10-03T21:52:57.181Z',
+      objectId: 'NhjOaLlsT2',
+      roomname: 'lobby',
+      text: 'hello there',
+      updatedAt:'2016-10-03T21:52:57.181Z',
+      username:'steve'
+    },
+    {
+      createdAt: '2016-10-03T21:52:49.965Z',
+      objectId: 'b0uzOcAMM0',
+      roomname: 'lobby',
+      text: 'kasdjfkjsadf',
+      updatedAt: '2016-10-03T21:52:49.965Z',
+      username: 'steve'
+    }
+  ]
+};
+
+module.exports = messages;
