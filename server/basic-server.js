5557ebbd8ca0bd377628650ea7d92a16084d4fdc
BHesseldieck <b.hesseldieck@gmail.com>
2016-10-04 11:55:43 -0700

    refactored to expressjs

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,41 +1,42 @@
-/* Import node's http module: */
-var http = require('http');
-var url = require('url');
-var handleRequest = require('./request-handler').requestHandler;
+var express = require('express');
+var path = require('path');
+var fs = require('fs');
+var app = express();
 
-// Every server needs to listen on a port with a unique number. The
-// standard port for HTTP servers is port 80, but that port is
-// normally already claimed by another server and/or not accessible
-// so we'll use a standard testing port like 3000, other common development
-// ports are 8080 and 1337.
 var port = 3000;
-
-// For now, since you're running this server on your local machine,
-// we'll have it listen on the IP address 127.0.0.1, which is a
-// special address that always refers to localhost.
 var ip = '127.0.0.1';
 
+app.listen(port, ip, function() {
+  console.log('Listening on http://' + ip + ':' + port);
+});
 
+app.use(function(req, res, next) {
+  res.header('access-control-allow-origin', '*');
+  res.header('access-control-allow-methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('access-control-allow-headers', 'content-type, accept');
+  res.header('access-control-max-age', 10);
+  next();
+});
 
-// We use node's http module to create a server.
-//
-// The function we pass to http.createServer will be used to handle all
-// incoming requests.
-//
-// After creating the server, we will tell it to listen on the given port and IP. */
-var server = http.createServer(handleRequest);
-console.log('Listening on http://' + ip + ':' + port);
-server.listen(port, ip);
+app.get('/', function(req, res) {
+  res.status(200).sendFile(path.join(__dirname, '../client/client/index.html'));
+});
 
-// To start this server, run:
-//
-//   node basic-server.js
-//
-// on the command line.
-//
-// To connect to the server, load http://127.0.0.1:3000 in your web
-// browser.
-//
-// server.listen() will continue running as long as there is the
-// possibility of serving more requests. To stop your server, hit
-// Ctrl-C on the command line.
+app.get('/classes/messages', function(req, res) {
+  res.status(200).sendFile(path.join(__dirname, '/messages.json'));
+});
+
+app.post('/classes/messages', function(req, res) {
+  req.on('data', data => { 
+    var newMessage = JSON.parse(data);
+    // TODO: chain read and write with next
+    fs.readFile(path.join(__dirname, '/messages.json'), (err, data) => {
+      var messages = JSON.parse(data);
+      messages.results.unshift(newMessage);
+
+      fs.writeFile(path.join(__dirname, '/messages.json'), JSON.stringify(messages), () => {
+        res.status(201).end();
+      });
+    });
+  });
+});
