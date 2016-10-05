19adc4063db6ea03273f08b828b07ee09afb6083
BHesseldieck <b.hesseldieck@gmail.com>
2016-10-04 19:45:12 -0700

    optimized Sort, commented, ES6 refactor

--- benchmark.js
+++ benchmark.js
@@ -3,6 +3,6 @@ var siege = require('siege');
 siege()
   .on(3000)
   .for(100000).times
-  .concurrent(100)
+  .concurrent(1000)
   .get('/classes/messages')
   .attack();
--- server/basic-server.js
+++ server/basic-server.js
@@ -1,8 +1,9 @@
 const cluster = require('cluster');
 
 if (cluster.isMaster) {
+  // get CPUs
   const numCPUs = require('os').cpus().length;
-  
+  // put worker on each CPU
   for (var i = 0; i < numCPUs; i++) {
     cluster.fork();
   }
@@ -14,43 +15,55 @@ if (cluster.isMaster) {
   
   const port = 3000;
   const ip = '127.0.0.1';
-  
-  app.listen(port, ip, function() {
+  // start server
+  app.listen(port, ip, () => {
     console.log('Listening on http://' + ip + ':' + port);
     console.log('Worker %d running!', cluster.worker.id);
   });
-
-  app.use(function(req, res, next) {
+  // create Header to prevent CORS
+  app.use((req, res, next) => {
     res.header('access-control-allow-origin', '*');
     res.header('access-control-allow-methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('access-control-allow-headers', 'content-type, accept');
     res.header('access-control-max-age', 10);
     next();
   });
-
+  // handle root request -> sending index.html and answer all following requests of that
   app.use(express.static(path.join(__dirname, '../client/client')));
-
-  app.get('/classes/messages', function(req, res) {
+  // handle get request on API
+  app.get('/classes/messages', (req, res) => {
+    //read and prepare file for sending
     fs.readFile(path.join(__dirname, '/messages.json'), (err, data) => {
+
       var messages = JSON.parse(data);
-      messages.results.sort(function(a, b) {
-        if (req.query.order === '-createdAt') {
-          // newest message last
-          return new Date(a.createdAt) - new Date(b.createdAt);
-        } else if (req.query.order === '+createdAt') {
-          // newest message first
-          return new Date(b.createdAt) - new Date(a.createdAt);
-        }
-      });
+
+      if (req.query !== undefined) {
+        messages.results.sort((a, b) => {
+          if (req.query.order === '-createdAt') {
+            // newest message last
+            return new Date(a.createdAt) - new Date(b.createdAt);
+          } else if (req.query.order === '+createdAt') {
+            // newest message first
+            return new Date(b.createdAt) - new Date(a.createdAt);
+          }
+        });
+      }
+
       res.json(messages);
     });
   });
 
-  app.post('/classes/messages', function(req, res) {
+  app.post('/classes/messages', (req, res) => {
+
+    var newMessage;
+    // when receiving data start saving it and adding a timestamp
     req.on('data', data => { 
-      var newMessage = JSON.parse(data);
+      newMessage = JSON.parse(data);
       d = new Date();
       newMessage.createdAt = d.toISOString();
+    });
+    // when data stream has ended -> read, update, write, end
+    req.on('end', () => {
       fs.readFile(path.join(__dirname, '/messages.json'), (err, data) => {
         var messages = JSON.parse(data);
         messages.results.unshift(newMessage);
@@ -59,5 +72,6 @@ if (cluster.isMaster) {
         });
       });
     });
+
   });
 }
